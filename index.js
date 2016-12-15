9615edc18e56f8dcce6b39e4770a4a6c87b3c3eb
balazs4 <balazs4web@gmail.com>
2016-12-15 20:34:21 +0100

    write mp3,png and json to the drive

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const {exec, execSync} = require('child_process');
 const {join} = require('path');
+const {writeFileSync, existsSync} = require('fs');
 const app = require('express')();
 
 app.use(require('body-parser').urlencoded({ extended: true, limit: '1mb' }));
 app.use(require('body-parser').json({ extended: true, limit: '1mb' }));
+
+const blacklist = [/\s/g, /!/g, /&/g, /\//g, /'/g, /"/g];
 
 app.post('/deezer',
     (req, res, next) => {
@@ -11,14 +14,16 @@ app.post('/deezer',
         next();
     },
     (req, res) => {
-        const {artist, title, album} = req.body;
-        const name = `${artist}.${album}.${title}`
-            .replace(/\s/g, '')
-            .replace(/!/g,'')
-            .replace(/&/g,'')
-            .replace(/\//g, '');
-        console.log(name);
-        exec(`./bin/record2mp3 ${name}`)
+        const {artist, title, album, cover} = req.body;
+        const name = blacklist.reduce((acc, next) => acc.replace(next, ''), `${artist}.${album}.${title}`);
+        const [audio, picture, metadata] = ['mp3', 'png', 'json'].map(ext => join(__dirname, 'content', `${name}.${ext}`));
+
+        if (existsSync(audio) === false) {
+            exec(`./bin/record2mp3 ${audio}`);
+            writeFileSync(`${picture}`, cover.replace(/^data:image\/png;base64,/, ''), 'base64');
+            writeFileSync(`${metadata}`, JSON.stringify({ artist, title, album }));
+        }
+
         res.sendStatus(202);
     })
 
