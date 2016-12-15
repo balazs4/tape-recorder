5c4fe0d8c78c76e5ea935c7bcb2194e933621ae1
balazs4 <balazs4web@gmail.com>
2016-12-15 21:19:34 +0100

    reenable recording

--- index.js
+++ index.js
@@ -6,26 +6,7 @@ const app = require('express')();
 app.use(require('body-parser').urlencoded({ extended: true, limit: '1mb' }));
 app.use(require('body-parser').json({ extended: true, limit: '1mb' }));
 
-const blacklist = {
-    á: 'a',
-    é: 'e',
-    ú: 'u',
-    ő: 'o',
-    ű: 'u',
-    ü: 'u',
-    ö: 'o',
-    ó: 'o',
-    í: 'i',
-    Á: 'A',
-    É: 'E',
-    Ú: 'U',
-    Ő: 'O',
-    Ó: 'O',
-    Ü: 'U',
-    Ö: 'O',
-    Í: 'I',
-    Ű: 'U'
-}
+const blacklist = { á: 'a', é: 'e', ú: 'u', ő: 'o', ű: 'u', ü: 'u', ö: 'o', ó: 'o', í: 'i', Á: 'A', É: 'E', Ú: 'U', Ő: 'O', Ó: 'O', Ü: 'U', Ö: 'O', Í: 'I', Ű: 'U' }
 
 app.post('/deezer',
     (req, res, next) => {
@@ -37,12 +18,12 @@ app.post('/deezer',
         const name = Object
             .keys(blacklist)
             .reduce((acc, ch) => acc.replace(new RegExp(ch, 'g'), blacklist[ch]),
-            `${artist}.${album}.${title}`.replace(/\s|!|&|\/|'|"/g, '')
+            `${artist}.${album}.${title}`.replace(/\s|!|&|\/|'|"|(|)/g, '')
             );
         const [audio, picture, metadata] = ['mp3', 'png', 'json'].map(ext => join(__dirname, 'content', `${name}.${ext}`));
 
         if (existsSync(audio) === false) {
-            //exec(`./bin/record2mp3 ${audio}`);
+            exec(`./bin/record2mp3 ${audio}`);
             writeFileSync(`${picture}`, cover.replace(/^data:image\/png;base64,/, ''), 'base64');
             writeFileSync(`${metadata}`, JSON.stringify({ artist, title, album }));
         }
